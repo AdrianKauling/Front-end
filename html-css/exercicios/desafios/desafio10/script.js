4bf7346d712d1c1babebb6f3e4a0281694729dbb
AdrianKauling <adriankauling2@gmail.com>
2022-01-06 21:55:47 -0300

    Finalizado o gerador de horarios

--- html-css/exercicios/desafios/desafio10/script.js
+++ html-css/exercicios/desafios/desafio10/script.js
@@ -1,4 +1,47 @@
+let controleLista = false
+
+
 function gerarHorario() {
-    const textHoras = document.getElementById('campoHoras').innerText
-    const horaGerada = Math.random()
+    const textHoras = document.getElementById('campoHoras')
+    let horaGerada = String(gerarNumeroAleatorio(0,23))
+    let minutosGerados = String(gerarNumeroAleatorio(0,59))
+
+    if(horaGerada.length === 1) {
+        horaGerada = '0' + horaGerada
+    }
+
+    if(minutosGerados.length === 1) {
+        minutosGerados = '0' + minutosGerados
+    }
+    
+    textHoras.innerText = `${horaGerada}:${minutosGerados}`
 }
+
+
+
+function gerarNumeroAleatorio( menorNumero, maiorNumero){
+
+    return Math.floor(Math.random() * (maiorNumero - menorNumero + 1)) + menorNumero
+}
+
+// function listarTermos() {
+//     const ListaDeTermos = document.getElementById('listaDeTermos')
+//     controleLista = controleLista? false:true 
+
+//     if(controleLista){
+    
+//     const ld = document.createElement('ld')
+
+//     const DTeDD = `<dt>Wake Up</dt> <dd>É o verbo 'acordar'</dd>
+//     <dt>Perguntando as horas</dt><dd>Could you tell me the time</dd>
+//     <dt>Resposta 1</dt><dd>it's fifteen to ten pm</dd>
+//     <dt>Resposta 2</dt><dd>It's a quater to ten pm</dd>
+//     <dt>Resposta 3</dt><dd>It's nine forty-five</dd>
+//     `
+//     ld.innerHTML = DTeDD
+//     ListaDeTermos.appendChild(ld)
+// }else{
+//     ListaDeTermos.innerHTML = ''
+// }
+
+// }
